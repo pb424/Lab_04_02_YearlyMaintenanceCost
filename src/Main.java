64d230561be2c072f9848cf66c7fa0a0ec1f02f9
paxton <clucksenor@gmail.com>
2025-09-28 21:28:44 -0400

    Init commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        double winterCost = 100.5;
+        double springCost = 55.2;
+        double summerCost = 79.6;
+        double fallCost = 67.1;
+        double totalCost = winterCost + springCost + summerCost + fallCost;
+        System.out.println("The total cost for the year is $" + totalCost + ", with a cost of $" + winterCost + " for Winter, a cost of $" + springCost + " for Spring, a cost of $" + summerCost + " for Summer, and a cost of $" + fallCost + " for Fall." );
+    }
+}
